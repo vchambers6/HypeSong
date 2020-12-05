9bd72bfd115b9aeb633975dbc86829f6fcfdb7cf
vchambers6 <vanessachambers6@gmail.com>
2020-12-05 01:35:08 -0600

    updated UI

---
+++ HypeSong/DiscoverPlaylist.swift
@@ -0,0 +1,9 @@
+//
+//  DiscoverPlaylist.swift
+//  HypeSong
+//
+//  Created by Vanessa Chambers on 12/2/20.
+//  Copyright © 2020 Vanessa Chambers. All rights reserved.
+//
+
+import Foundation
---
+++ HypeSong/LoadingButton.swift
@@ -0,0 +1,9 @@
+//
+//  LoadingButton.swift
+//  HypeSong
+//
+//  Created by Vanessa Chambers on 12/3/20.
+//  Copyright © 2020 Vanessa Chambers. All rights reserved.
+//
+
+import Foundation
