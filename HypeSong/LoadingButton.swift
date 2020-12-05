c903b3458dab2d1f064a8cd519cb85e0c5245454
vchambers6 <vanessachambers6@gmail.com>
2020-12-05 01:36:16 -0600

    added loading spinner to button

--- HypeSong/LoadingButton.swift
+++ HypeSong/LoadingButton.swift
@@ -2,8 +2,54 @@
 //  LoadingButton.swift
 //  HypeSong
 //
-//  Created by Vanessa Chambers on 12/3/20.
-//  Copyright © 2020 Vanessa Chambers. All rights reserved.
 //
 
-import Foundation
+// Code snippet from
+// https://stackoverflow.com/questions/36539650/display-activity-indicator-inside-uibutton
+// Modified for use on HypeSong
+
+import UIKit
+class LoadingButton: UIButton {
+    private var originalButtonText: String?
+    var activityIndicator: UIActivityIndicatorView!
+
+    func showLoading() {
+        originalButtonText = self.titleLabel?.text
+        self.setTitle("", for: .normal)
+        
+        if (activityIndicator == nil) {
+            activityIndicator = createActivityIndicator()
+        }
+        
+        showSpinning()
+    }
+
+    func hideLoading() {
+        self.setTitle(originalButtonText, for: .normal)
+        activityIndicator.stopAnimating()
+    }
+
+    private func createActivityIndicator() -> UIActivityIndicatorView {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.color = .white
+//        Increases size of activity indicator
+//        activityIndicator.transform = CGAffineTransform(scaleX: 5, y: 5)
+        return activityIndicator
+    }
+
+    private func showSpinning() {
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(activityIndicator)
+        centerActivityIndicatorInButton()
+        activityIndicator.startAnimating()
+    }
+
+    private func centerActivityIndicatorInButton() {
+        let xCenterConstraint = NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: activityIndicator, attribute: .centerX, multiplier: 1, constant: 0)
+        self.addConstraint(xCenterConstraint)
+        
+        let yCenterConstraint = NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: activityIndicator, attribute: .centerY, multiplier: 1, constant: 0)
+        self.addConstraint(yCenterConstraint)
+    }
+}
